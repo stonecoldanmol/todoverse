2212a05bd3345e45046b9b993b3cfdb9d502611b
Anmol Sinha <sinhaanmol.16@gmail.com>
2020-10-31 23:12:33 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/anmolsinha/todoverse/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.anmolsinha.todoverse
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/homepage.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:todoverse/widgets.dart';
+
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(
+            horizontal: 24.0,
+            vertical: 32.0,
+          ),
+          color: Color(0xFFF6F6F6),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:[
+              Container(
+                margin: EdgeInsets.only(
+                  bottom: 32.0
+                ),
+                child: Image(
+                    image: AssetImage(
+                      'assets/images/logo.png'
+                    ),
+                ),
+              ),
+              TaskCardWidget(),
+              TaskCardWidget(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
