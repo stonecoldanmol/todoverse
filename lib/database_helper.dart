a83e86acb4453cee7c131a4af57c42ee57d9598d
Anmol Sinha <sinhaanmol.16@gmail.com>
2020-11-13 12:06:26 +0530

    updating the database

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -1,7 +1,26 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:todoverse/models/task.dart';
+
 class DatabaseHelper
 {
+Future<Database> database() async
+{
+  return openDatabase(
+    join(await getDatabasesPath(),'todo.db'),
+    onCreate: (db, version) async {
+      await db.execute("CREATE TABLE tasks(id INTEGER PRIMARY KEY, title TEXT, description TEXT)");
+      await db.execute("CREATE TABLE todo(id INTEGER PRIMARY KEY, taskId INTEGER, title TEXT, isDone INTEGER)");
+      return db;
+    },
+    version: 1,
+  );
+}
 
-
-
+  Future<void> insertTask(Task task) async
+  {
+    Database _db=await database();
+    await _db.insert('tasks', task.toMap(),conflictAlgorithm: ConflictAlgorithm.replace);
+  }
 
 }
