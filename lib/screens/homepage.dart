b2d93dc9f96b448bf83df4fd72e8a05f8039684c
Anmol Sinha <sinhaanmol.16@gmail.com>
2020-12-11 16:49:33 +0530

    Added almost everything

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:todoverse/database_helper.dart';
 import 'package:todoverse/screens/taskpage.dart';
 import 'package:todoverse/widgets.dart';
@@ -10,10 +9,8 @@ class Homepage extends StatefulWidget {
   _HomepageState createState() => _HomepageState();
 }
 
-class _HomepageState extends State<Homepage>
-{
-
-  DatabaseHelper _dbHelper=DatabaseHelper();
+class _HomepageState extends State<Homepage> {
+  DatabaseHelper _dbHelper = DatabaseHelper();
 
   @override
   Widget build(BuildContext context) {
@@ -21,44 +18,50 @@ class _HomepageState extends State<Homepage>
       body: SafeArea(
         child: Container(
           width: double.infinity,
-          padding: EdgeInsets.symmetric(
-            horizontal: 24.0,
-          ),
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
           color: Color(0xFFF6F6F6),
           child: Stack(
-            children: <Widget>[
+            children: [
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children:[
+                children: [
                   Container(
                     margin: EdgeInsets.only(
                       top: 32.0,
-                      bottom: 32.0
+                      bottom: 32.0,
                     ),
                     child: Image(
-                        image: AssetImage(
-                          'assets/images/logo.png'
-                        ),
+                      image: AssetImage('assets/images/logo.png'),
                     ),
                   ),
                   Expanded(
                     child: FutureBuilder(
                       initialData: [],
                       future: _dbHelper.getTasks(),
-                      builder: (context,snapshot){
+                      builder: (context, snapshot) {
                         return ScrollConfiguration(
                           behavior: NoGlowBehaviour(),
                           child: ListView.builder(
                             itemCount: snapshot.data.length,
-                            itemBuilder: (context,index){
+                            itemBuilder: (context, index) {
                               return GestureDetector(
-                                onTap: (){
-                                  Navigator.push(context, MaterialPageRoute(builder: (context)=>TaskPage(
-                                    task:snapshot.data[index],
-                                    )));
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => Taskpage(
+                                        task: snapshot.data[index],
+                                      ),
+                                    ),
+                                  ).then(
+                                        (value) {
+                                      setState(() {});
+                                    },
+                                  );
                                 },
                                 child: TaskCardWidget(
                                   title: snapshot.data[index].title,
+                                  desc: snapshot.data[index].description,
                                 ),
                               );
                             },
@@ -73,34 +76,35 @@ class _HomepageState extends State<Homepage>
                 bottom: 24.0,
                 right: 0.0,
                 child: GestureDetector(
-                  onTap: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>TaskPage(task: null,)),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => Taskpage(
+                            task: null,
+                          )),
                     ).then((value) {
-                      setState(() {
-
-                      });
+                      setState(() {});
                     });
                   },
-
                   child: Container(
-                    width: 60,
-                    height: 60,
+                    width: 60.0,
+                    height: 60.0,
                     decoration: BoxDecoration(
                       gradient: LinearGradient(
-                          colors: [Color(0xFF7349FE),Color(0xFF643FDE)],
-                        begin: Alignment(0.0,-1.0),
-                        end:Alignment(0.0,1.0),
-                      ),
+                          colors: [Color(0xFF7349FE), Color(0xFF643FDB)],
+                          begin: Alignment(0.0, -1.0),
+                          end: Alignment(0.0, 1.0)),
                       borderRadius: BorderRadius.circular(20.0),
                     ),
                     child: Image(
-                        image: AssetImage(
-                          'assets/images/add_icon.png',
-                        )
+                      image: AssetImage(
+                        "assets/images/add_icon.png",
+                      ),
                     ),
                   ),
                 ),
-              ),
+              )
             ],
           ),
         ),
